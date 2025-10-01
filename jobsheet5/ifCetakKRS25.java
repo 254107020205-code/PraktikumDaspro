341f96d1f01b4f14a30f69444feeb7fbe79005d3
254107020205 <254107020205@student.polinema.ac.id>
2025-10-01 11:33:51 +0700

    “Modifikasi Percobaan 1”

---
+++ jobsheet5/ifCetakKRS25.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ifCetakKRS25 {
+    
+public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+
+     System.out.print("--- Cetak KRS SIAKAD --- ");
+        System.out.print("Apakah UKT sudah lunas? (True/false): ");
+        boolean uktlunas = sc.nextBoolean();
+
+        if (uktlunas) {
+            System.out.print("Pembayaran UKT terverivikasi");
+            System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+        
+    }
+}
